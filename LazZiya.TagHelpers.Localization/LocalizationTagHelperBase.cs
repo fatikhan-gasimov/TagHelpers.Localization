3aa65f022a97e21caa586a321c8e4009081075f4
Ziya Mollamahmut <ziyadmm@gmail.com>
2019-06-13 19:00:43 +0300

    Add project files.

---
+++ LazZiya.TagHelpers.Localization/LocalizationTagHelperBase.cs
@@ -0,0 +1,49 @@
+﻿using LazZiya.ExpressLocalization;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace LazZiya.TagHelpers.Localization
+{
+    /// <summary>
+    /// base class for localize tag helpers
+    /// </summary>
+    public class LocalizationTagHelperBase : TagHelper
+    {
+        private readonly SharedCultureLocalizer _loc;
+
+        /// <summary>
+        /// pass array of objects for arguments
+        /// </summary>
+        [HtmlAttributeName("localize-args")]
+        public object[] Args { get; set; }
+
+        /// <summary>
+        /// inject SharedCultureLocalizer
+        /// </summary>
+        /// <param name="loc"></param>
+        public LocalizationTagHelperBase(SharedCultureLocalizer loc)
+        {
+            _loc = loc;
+        }
+        
+        /// <summary>
+        /// process localize tag helper
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="output"></param>
+        /// <returns></returns>
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var content = await output.GetChildContentAsync();
+
+            if (!string.IsNullOrWhiteSpace(content.GetContent()))
+            {
+                var str = content.GetContent().Trim();
+
+                var _localStr = _loc.Text(str, Args);
+
+                output.Content.SetHtmlContent(_localStr);
+            }
+        }
+    }
+}
---
+++ LazZiya.TagHelpers.Localization/LocalizeAttributeTagHelper.cs
@@ -0,0 +1,41 @@
+﻿using LazZiya.ExpressLocalization;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace LazZiya.TagHelpers.Localization
+{
+    /// <summary>
+    /// Localization tag helper, localize text inside <![CDATA[<localize>Hellow</localize>]]>
+    /// </summary>
+    /// 
+    [HtmlTargetElement(Attributes = "localize*")]
+    public class LocalizeAttributeTagHelper : LocalizationTagHelperBase
+    {
+        /// <summary>
+        /// Bool value, true to localize content
+        /// </summary>
+        [HtmlAttributeName("localize-content")]
+        public bool Localize { get; set; } = true;
+
+        /// <summary>
+        /// inject SharedCultureLocalizer
+        /// </summary>
+        /// <param name="loc"></param>
+        public LocalizeAttributeTagHelper(SharedCultureLocalizer loc) : base(loc)
+        {
+        }
+        /// <summary>
+        /// process localize tag helper
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="output"></param>
+        /// <returns></returns>
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (Localize)
+            {
+                await base.ProcessAsync(context, output);
+            }
+        }
+    }
+}
